5031992eb87f77a48df27f8a5af9d9a750b8db81
Ziv Sion <zivsion7106@gmail.com>
2022-10-03 18:47:04 +0300

    Search bar changed

--- app/src/main/java/com/example/locationalarm/MainActivity.java
+++ app/src/main/java/com/example/locationalarm/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.locationalarm;
 
+import static android.service.controls.ControlsProviderService.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -7,13 +9,21 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.SearchView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -28,35 +38,93 @@ public class MainActivity extends AppCompatActivity {
 
     HashMap<String, ItemData> data = new HashMap<>();
     ArrayList<ItemData> dataArrayList = new ArrayList<>();
+    ArrayList<ItemData> fixedData = new ArrayList<>();
+
+    TextInputEditText searchBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        searchBox = findViewById(R.id.searchBox);
 
-        // 1. Read from file
-        // 2. Add to map
+
+        // Read from file and add to tha map
         try {
             data = LoadData(FILE);
         } catch (IOException ignored) {
         }
 
-        // 3. Add to array from map
+        // Create an array from the data in the map
         dataArrayList = dataAsArray(data);
 
         // for testing:
-        dataArrayList.add(new ItemData("test", "addr", "38.55", "36.88", "100", "ring1"));
-        dataArrayList.add(new ItemData("test2", "ad2dr", "38.554444444", "36.4444488", "134340", "ring1"));
-        dataArrayList.add(new ItemData("tesat2", "ad2dr", "38.554444444", "36.4488", "134", "ring1"));
+        dataArrayList.add(new ItemData("test", "Israel, Nofey Prat", "38.55", "36.88", "100", "ring1"));
+        dataArrayList.add(new ItemData("test2", "USA", "38.554444444", "36.4444488", "134340", "ring1"));
+        dataArrayList.add(new ItemData("test3", "England", "38.554444444", "36.4488", "134", "ring1"));
+
+        // * fixedData will never change - used to filter
+        fixedData = new ArrayList<>(dataArrayList);
+
         // 4. Set adapter
-        initRecyclerView(dataArrayList, this);
+        initRecyclerView(this);
         initSearchRecycler();
     }
 
+    /**
+     * when search bar is used, update the recycler view to show only the items that match the search
+     */
+    private void initSearchRecycler() {
+        // set onclick listener
+        searchBox.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // S is new content
+                // Filter data array
+
+                int index = 0;
+
+                for (ItemData item : fixedData) {
+                    Log.d(TAG, "afterTextChanged: Checking item: " + item.getName());
+                    if (item.getName().toLowerCase().contains(String.valueOf(s).toLowerCase()) ||
+                            item.getAddress().toLowerCase().contains(String.valueOf(s).toLowerCase()) ||
+                            item.getLatitude().toLowerCase().contains(String.valueOf(s).toLowerCase()) ||
+                            item.getLongitude().toLowerCase().contains(String.valueOf(s).toLowerCase())) {
+
+                        if (!dataArrayList.contains(item)) {
+                            dataArrayList.add(item);
+                        }
+
+                    } else {
+                        if (dataArrayList.contains(item)) {
+                            dataArrayList.remove(item);
+                        }
+                    }
+
+                    index++;
+                }
+
+                // Update
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+
     /*
     turn a hashmap of string and itemdata into an arraylist of itemdata
-     */
+    */
     private ArrayList<ItemData> dataAsArray(HashMap<String, ItemData> data) {
         ArrayList<ItemData> temp = new ArrayList<>();
 
@@ -103,43 +171,13 @@ public class MainActivity extends AppCompatActivity {
 
 
     /**
-     * @param data    array of data to be displayed
      * @param context context of the activity
      */
-    private void initRecyclerView(ArrayList<ItemData> data, Context context) {
+    private void initRecyclerView(Context context) {
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        RecyclerAdapter adapter = new RecyclerAdapter(data, context);
+        adapter = new RecyclerAdapter(dataArrayList, context);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(context));
     }
-
-
-    /**
-     * when search bar is used, update the recycler view to show only the items that match the search
-     */
-    private void initSearchRecycler() {
-        SearchView searchView = (SearchView) findViewById(R.id.searchView);
-        // set onclick listener
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) { // change text on search
-                ArrayList<ItemData> filterdList = new ArrayList<>();
-
-                for (ItemData entry : dataArrayList) {// find matching items
-                    if (entry.getName().toLowerCase().contains(newText.toLowerCase())) {
-                        filterdList.add(entry);
-                    }
-                }
-                initRecyclerView(filterdList, getApplicationContext()); // update recycler view
-                return false;
-            }
-        });
-    }
-
 
 }
