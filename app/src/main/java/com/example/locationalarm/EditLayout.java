ce0e72554f4cadf3308bd32418f0b167fedc89e7
Ziv Sion <zivsion7106@gmail.com>
2022-10-04 13:01:00 +0300

    Total and active locations textview in activity_main.xml

--- app/src/main/java/com/example/locationalarm/EditLayout.java
+++ app/src/main/java/com/example/locationalarm/EditLayout.java
@@ -21,6 +21,8 @@ public class EditLayout extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_layout);
 
+        getSupportActionBar().hide();
+
         initViews();
     }
 
--- app/src/main/java/com/example/locationalarm/MainActivity.java
+++ app/src/main/java/com/example/locationalarm/MainActivity.java
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
 
         initViews();
 
