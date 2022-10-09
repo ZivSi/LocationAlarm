57ce280f8ad4c44baf73f2cd50f1ea2d5f73dccf
matan kabalo <matanton666@gmail.com>
2022-10-09 11:46:21 +0300

    added ringtone to the settings but it still does not work

--- app/src/main/java/com/example/locationalarm/Settings.java
+++ app/src/main/java/com/example/locationalarm/Settings.java
@@ -1,13 +1,18 @@
 package com.example.locationalarm;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
+import android.media.RingtoneManager;
+import android.util.Log;
 
 import java.util.Map;
 import java.util.Objects;
@@ -38,6 +43,12 @@ public class Settings extends AppCompatActivity {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+            Preference ringtone = findPreference("ringtone");
+            Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+            assert ringtone != null;
+            ringtone.setIntent(intent); // fixme: when picking a ringtone it just closes and doesn't set the ringtone
+            // todo: get the ringtone chosen by the user and save in variable
         }
     }
 
@@ -45,6 +56,18 @@ public class Settings extends AppCompatActivity {
     public static void getInfo(Context ct){
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(ct);
         allEntries = sp.getAll();
+    }
+
+    public void ringtone(){
+        RingtoneManager ringtoneManager = new RingtoneManager(this);
+        ringtoneManager.setType(RingtoneManager.TYPE_RINGTONE);
+        Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_RINGTONE);
+        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "Select Ringtone");
+        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI, (android.net.Uri) null);
+        startActivity(intent);
+        ringtoneManager.getRingtone(0);
+        Log.d("ringtone", ringtoneManager.getRingtone(0).toString());
     }
 }
 
