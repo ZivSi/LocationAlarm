e45018fa21b519d688b06e1dbf3db75762a3ed70
matan kabalo <matanton666@gmail.com>
2022-10-10 20:14:18 +0300

    added to location class functions to get the distance between the user and the destination

--- app/src/main/java/com/example/locationalarm/LocationFinder.java
+++ app/src/main/java/com/example/locationalarm/LocationFinder.java
@@ -16,17 +16,19 @@ import com.google.android.gms.tasks.Task;
 
 public class LocationFinder {
     // init variables
-    private String latitude;
-    private String longitude;
-    private String destaddress;
+    public String latitude;
+    public String longitude;
+
     private String destlatitude;
     private String destlongitude;
+
     private FusedLocationProviderClient fusedLocationClient;
+    Context context;
 
     public LocationFinder(String destLongitude, String destLatitude, Context context) {
         this.destlatitude = destLatitude;
         this.destlongitude = destLongitude;
-
+        this.context = context;
         this.fusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
     }
 
@@ -47,7 +49,7 @@ public class LocationFinder {
 
     // get the current location of the user
     @SuppressLint("MissingPermission")
-    public String[] getUserLocation(Context context){
+    private String[] getUserLocation(){
         if(getLocationPermission(context)) {
             // check if permission is granted
             fusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
@@ -75,5 +77,42 @@ public class LocationFinder {
 
     public String getLongitude() {
         return longitude;
+    }
+
+
+    /**
+     * Calculate distance between two points in latitude and longitude . If you are not interested in height
+     * difference pass 0.0. Uses Haversine method as its base.
+     *
+     * lat1, lon1 Start point lat2, lon2 End point
+     * @return Distance in Meters
+     */
+    private double calcDistance(double lat1, double lat2, double lon1, double lon2) {
+
+        final int R = 6371; // Radius of the earth
+
+        double latDistance = Math.toRadians(lat2 - lat1); // turn degrees into radians
+        double lonDistance = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) // take in the earth curvature to the equation
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return (R * c * 1000); // convert to meters
+    }
+
+
+    // get the absolute distance between the user and the destination
+    public int getDistanceFromUserToDestination(){
+        if (getLocationPermission((context))){
+            // get the current location of the user
+            String[] userLocation = getUserLocation();
+            double long1 = Double.parseDouble(userLocation[0]);
+            double lat1 = Double.parseDouble(userLocation[1]);
+            // calculate the distance between the user and the destination
+            double distance = calcDistance(lat1, Double.parseDouble(destlatitude), long1, Double.parseDouble(destlongitude));
+            return Math.abs((int) distance); // absolute value of the distance
+        }
+        return 0;
     }
 }
