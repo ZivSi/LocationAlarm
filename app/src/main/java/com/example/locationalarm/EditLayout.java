22dba6fc34ae2da100872f0e6abe2cad91487e63
matan kabalo <matanton666@gmail.com>
2022-10-30 21:20:34 +0200

    added backend for the address box and fixed geolocator in it

--- app/src/main/java/com/example/locationalarm/EditLayout.java
+++ app/src/main/java/com/example/locationalarm/EditLayout.java
@@ -7,15 +7,13 @@ import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.SeekBar;
-import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.material.button.MaterialButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -23,13 +21,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
-import java.util.Optional;
 
 public class EditLayout extends AppCompatActivity {
-    boolean isEdit = false;
+    boolean isEdit = false, isAddress = true;
     // main variables
-    TextInputEditText nameBox, xCoordiantesBox, yCoordinatesBox;
+    TextInputEditText nameBox, xCoordiantesBox, yCoordinatesBox, addressBox;
     TextView distanceTextView;
     SeekBar seekBar;
 
@@ -41,7 +37,6 @@ public class EditLayout extends AppCompatActivity {
     // Get address from coordinates
     Geocoder geocoder;
     List<Address> addresses = new ArrayList<>();
-    String country = "";
     String address = "";
 
     ItemData editItem;
@@ -55,6 +50,7 @@ public class EditLayout extends AppCompatActivity {
         // get extras
         String name;
         Bundle extras = getIntent().getExtras();
+
         if (extras != null) {
             isEdit = true;
             name = extras.getString("name");
@@ -70,16 +66,20 @@ public class EditLayout extends AppCompatActivity {
         nameBox = findViewById(R.id.nameBox);
         xCoordiantesBox = findViewById(R.id.xCoordinatesBox);
         yCoordinatesBox = findViewById(R.id.yCoordinatesBox);
+        addressBox = findViewById(R.id.addressBox);
         distanceTextView = findViewById(R.id.distanceTextView);
         seekBar = findViewById(R.id.seekBar);
         layout = findViewById(R.id.mainEditLayout);
         snackbar = Snackbar.make(layout, "", Snackbar.LENGTH_SHORT);
         View custom_view = getLayoutInflater().inflate(R.layout.snackbar_error, null);
+        findViewById(R.id.xCoordinatesLayout).setVisibility(View.INVISIBLE);
+        findViewById(R.id.yCoordinatesLayout).setVisibility(View.INVISIBLE);
 
         if (isEdit) { // if the user is editing an item set the values of the boxes to the current item
             nameBox.setText(editItem.getName());
-            xCoordiantesBox.setText(editItem.getLongitude());
-            yCoordinatesBox.setText(editItem.getLatitude());
+            addressBox.setText(editItem.getAddress());
+            xCoordiantesBox.setText(String.valueOf(editItem.getLongitude()));
+            yCoordinatesBox.setText(String.valueOf(editItem.getLatitude()));
             distanceTextView.setText(editItem.getAlarmDistance());
             seekBar.setProgress(getDistFromFullValue(Integer.parseInt(editItem.getAlarmDistance())));
             getDistance();
@@ -122,25 +122,26 @@ public class EditLayout extends AppCompatActivity {
      * Save the new data in file, after checked all properties are valid
      */
     public void saveData() {
+        String x, y;
 
         if (isEdit) { // remove the original and save the new one
             MainActivity.data.remove(editItem.getName());
         } ;
         geocoder = new Geocoder(this, Locale.getDefault());
 
+        String[] qord = extractAddressAndCoordinates();
+        x = qord[0];
+        y = qord[1];
+
         // get fields
         String name = nameBox.getText().toString().replaceAll(":", "");
-        String x = xCoordiantesBox.getText().toString();
-        String y = yCoordinatesBox.getText().toString();
-        int distance = getDistance();
-
-
         // check for empty fields
-        if (name.isEmpty() || x.isEmpty() || y.isEmpty()) {
+        if (name.isEmpty()) {
             ((TextView) snackbarView.findViewById(R.id.error_message)).setText("Please fill all the fields");
             snackbar.show();
             return;
         }
+        if (x.isEmpty() || y.isEmpty())return;
         // check if the name is already taken
         if (MainActivity.data.containsKey(name)) {
             ((TextView) snackbarView.findViewById(R.id.error_message)).setText("Name already exists");
@@ -148,25 +149,65 @@ public class EditLayout extends AppCompatActivity {
             return;
         }
 
-        // Get address
-        try {
-            addresses = geocoder.getFromLocation(Double.parseDouble(x), Double.parseDouble(y), 1);
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
-        }
-
-        // No data found
-        if (addresses.size() == 0) {
-            country = "Not Found";
-        } else {
-            country = addresses.get(0).getCountryName() + ", ";
-        }
+        int distance = getDistance();
+        // Get address from coordinates
 
         // address is curently empty untill we add the google maps api
-        MainActivity.data.put(name, new ItemData(name, country + address, x, y, String.valueOf(distance)));
+        MainActivity.data.put(name, new ItemData(name, address, x, y, String.valueOf(distance)));
 
         Functions.SaveData(this, MainActivity.data);
+        Toast.makeText(this, "Location Saved!", Toast.LENGTH_SHORT).show();
         finish();
+    }
+
+
+    /**
+     * extracts the address and coordinates from the boxes of the location settings
+     * that the user enterd, if the user entered an address it will get the coordinates
+     * from the address and vice versa
+     * @return the coordinates of the location
+     */
+    private String[] extractAddressAndCoordinates() {
+        String x = "", y = "";
+
+        if (isAddress) {
+            try { // try and extract the coordinates from the address
+                addresses = geocoder.getFromLocationName(addressBox.getText().toString(), 1);
+                if (addresses.size() == 0) { // no address found
+                    ((TextView) snackbarView.findViewById(R.id.error_message)).setText("Address not found");
+                    snackbar.show();
+                    return new String[]{x, y};
+                }
+                else if (addresses.size() > 0) { // get long and lad
+                    address = addresses.get(0).getAddressLine(0);
+                    x = String.valueOf(addresses.get(0).getLatitude());
+                    y = String.valueOf(addresses.get(0).getLongitude());
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // get the address from the coordinates
+        else {
+            x = xCoordiantesBox.getText().toString();
+            y = yCoordinatesBox.getText().toString();
+            try { // get address
+                addresses = geocoder.getFromLocation(Double.parseDouble(x), Double.parseDouble(y), 1);
+            }
+            catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+
+            // No data found
+            if (addresses.size() == 0) {
+                address = "Not Found";
+            } else {
+                address = addresses.get(0).getCountryName() + ", " + address;
+            }
+        }
+        return new String[]{x, y};
     }
 
     // get the distance from the seekbar, set it to correct value and update textview
@@ -222,5 +263,24 @@ public class EditLayout extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    // set the text inputs to coordinates
+    public void showCoordinatesInput(View view) {
+        // set input text visible
+        findViewById(R.id.xCoordinatesLayout).setVisibility(View.VISIBLE);
+        findViewById(R.id.yCoordinatesLayout).setVisibility(View.VISIBLE);
+        findViewById(R.id.addressLayout).setVisibility(View.INVISIBLE);
+        isAddress = false;
+    }
+
+    // set the text input to address
+    public void showAddressInput(View view) {
+        // set input text visible
+        findViewById(R.id.yCoordinatesLayout).setVisibility(View.INVISIBLE);
+        findViewById(R.id.xCoordinatesLayout).setVisibility(View.INVISIBLE);
+        findViewById(R.id.addressLayout).setVisibility(View.VISIBLE);
+        isAddress = true;
+    }
+    // TODO: make address text input auto complete
 
 }
